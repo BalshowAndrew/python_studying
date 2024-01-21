f42882eddcdd1be5f3161b7b171cd6900077ccfb
Andrew <balshowandrew@gmail.com>
2024-01-21 15:16:48 +0300

    related_lists_ex_01.py was added

--- algorithms/shultais/related_lists/related_lists_ex_01.py
+++ algorithms/shultais/related_lists/related_lists_ex_01.py
@@ -1,8 +1,8 @@
 class Node:
-    def __init__(self, key, value):
+    def __init__(self, key, value, next_node=None):
         self.key = key
         self.value = value
-        self.next_node = None
+        self.next_node = next_node
 
     def __str__(self):
         return self.key, self.value
@@ -18,12 +18,10 @@ class List:
         Добавление нового узла в конец связанного списка.
         Время работы O(N).
         """
-        # Если нет первого элемента, то создаем его и завершаем работу.
         if self.head is None:
             self.head = Node(key, value)
             return
         
-        # Перебираем поочереди все элементы, чтобы найти последний
         current = self.head
         while current.next_node is not None:
             current = current.next_node
@@ -31,15 +29,18 @@ class List:
         # Создаем ссылку для последнего элемента на новый
         current.next_node = Node(key, value)
 
-    # def find(self, key):
-    #     current = self.head
-    #     while current.next_node is not None:
-    #         if current.key == key:
-    #             return current.value
-    #         break
+    def find(self, key):
+        """
+        Ищет ячейку в связанном списке по значению key и возвращает value.
+        Если элемента с искомым key в списке нет, возвращает None.
+        """
+        current = self.head
+        while current is not None:
+            if current.key == key:
+                return current.value
+            current = current.next_node
+        return None
             
-        
-
     def __str__(self):
         """
         Возвращает все элементы связанного списка в виде строки.
@@ -55,38 +56,13 @@ class List:
         return values + "]"
 
 
-# node1 = Node(0, 75)
-#
-# node2 = Node(1, 12)
-# node1.next_node = node2
-#
-# node3 = Node(2, 28)
-# node2.next_node = node3
-#
-# node4 = Node(3, 6)
-# node3.next_node = node4
-#
-# print(node1.key)
-# print(node1.value)
-# print(node1.__str__())
-# print(node1.next_node.value)
-
-
 lst = List()
 
-lst.append(0, 75)
-lst.append(1, 12)
-lst.append(2, 28)
-lst.append(3, 6)
+lst.append(0, 'A')
+lst.append(1, 'B')
+lst.append(2, 'C')
+lst.append(3, 'D')
 print(lst)
-
-current = lst.head
-while current is not None:
-    # print(str(current.key) + ', ' + str(current.value))
-    print(current.key, current.value)
-    current = current.next_node
-
-
-# while current.next_node is not None:
-#     print(current.key, current.value)
+print(lst.find(2))
+print(lst.find(4))
 
